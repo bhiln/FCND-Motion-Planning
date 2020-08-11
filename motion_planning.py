718c5aea6b987af6fa039f59d91fd9f8662100a1
Brian Hilnbrand <brhiln@gmail.com>
2020-08-11 15:13:28 -0700

    Add: read lat0, lon0 from colliders

--- motion_planning.py
+++ motion_planning.py
@@ -120,6 +120,13 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
+        lat0 = None
+        lon0 = None
+        with open('colliders.csv') as f:
+            line = f.readline()
+            line = line.replace(',','').strip().split()
+            lat0 = float(line[1])
+            lon0 = float(line[3])
         
         # TODO: set home position to (lon0, lat0, 0)
 
